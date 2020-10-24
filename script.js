ccd04fe43c1a30e82681dd1adc7256215ae24eb1
Aradhya Sharma <55148309+aradhya7420@users.noreply.github.com>
2020-10-25 01:03:38 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,155 +1,172 @@
-var canvas = document.getElementById('canvas');
-canvas.width = window.innerWidth;
-canvas.height = 2000;
-var ctx = canvas.getContext('2d');
-var penColor = undefined;
-ctx.lineWidth = 4;
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
+var canvas = new fabric.Canvas('canvas');
+var cWidth = window.innerWidth;
+var cHeight = 3000;
+var penColor;
 var started = false;
 var mode = undefined;
-
-// create an in-memory canvas
-var memCanvas = document.createElement('canvas');
-memCanvas.width = canvas.width;
-memCanvas.height = canvas.height;
-var memCtx = memCanvas.getContext('2d');
-var points = [];
-
-//Adding event listeners
-canvas.addEventListener('mousedown', mouseDown, false);
-canvas.addEventListener('mousemove', mouseMove, false);
-canvas.addEventListener('mouseup', mouseUp, false);
-document.addEventListener('mouseup', mouseUp);
-canvas.addEventListener('mouseout', mouseUp);
-document.getElementById('colorchoice').addEventListener('click', showPallete);
-canvas.addEventListener('mousedown', hidePallete);
+var obj = [];
+var lastStep = undefined;
+var visible = false;
 
 
-document.getElementById('clearCanvas').onclick = () => {     //Clear Canvas
+
+//Canvas Settings
+canvas.setDimensions({width:cWidth, height:cHeight});
+canvas.isDrawingMode = true;
+canvas.freeDrawingBrush.width = 5;
+canvas.freeDrawingBrush.color = 'black';
+canvas.allowTouchScrolling = 'true';
+mode = 'draw';
+
+
+//Sets color of pen
+function setColor(event) {
+    mode = 'draw';
+    canvas.isDrawingMode = true;
+    canvas.freeDrawingBrush.color = event.target.value;
+}
+
+// canvas.on('object:added', (e) => {
+//     if(obj.length >= 50) {
+//         obj = obj.slice(20, obj.length);
+//         obj.push(e.target);
+//         return;
+//     }
+//     else {
+//         obj.push(e.target);
+//     }
+//     console.log("Added => ", obj);
+// });
+
+// canvas.on('object:removed', (e) => {
+//     if(obj.length >= 50) {
+//         obj = obj.slice(20, obj.length);
+//         obj.push(e.target);
+//         return;
+//     }
+//     else {
+//         obj.push(e.target);
+//     }
+//     console.log("Removed => ", e.target);
+//     console.log("Undostack => ", obj);
+// });
+
+
+//Clear Handler
+function clearCanvas() {
+    obj = canvas.getObjects();
     var check = confirm('Do you want to clear the board?');
     if(check) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        memCtx.clearRect(0, 0, memCanvas.width, memCanvas.height);
+        var objClear = canvas.getObjects();
+        objClear.forEach((obj) => {
+            canvas.remove(obj);
+        })
+        obj.push('clear');
+        console.log(obj);
     }
 }
 
+//Shows color pallete
+document.getElementById('draw').addEventListener('click', togglePallete);
 
-document.getElementById('eraser').onclick =  () => {        //Eraser
-    mode = 'erase';
-    drawPoints(ctx, points, e);
-};
+//Clears canvas when "Clear" button is clicked
+document.getElementById('clear').addEventListener('click', clearCanvas);
 
-function showPallete() {
-    if(mode !== 'draw') {
-        mode = 'draw';
+var redo= [];
+
+//Undo Handler
+document.getElementById('undo').addEventListener('click', () => { 
+    mode = 'undo';
+    if(obj[obj.length - 1] === 'clear') {
+        obj.pop();
+        obj.forEach((obj) => {
+            canvas.add(obj);
+        })
+        obj = [];
+        redo.push('clear');
+        console.log('afterClear-undo', obj);
         return;
-    }    
+    }
+    obj = canvas.getObjects();
+
+    if(obj.length !== 0) {
+        redo.push(obj[obj.length - 1]);
+        canvas.remove(obj[obj.length - 1]);
+        obj.pop();
+    }
+});
+
+
+//Redo Handler
+document.getElementById('redo').addEventListener('click', () => {    
+    mode = 'redo';
+    console.log('Redo Stack = ', obj);
+    try {
+        if(redo.length !== 0) {
+            if(redo.length >= 50) {
+                redo.slice(10, redo.length);
+            }
+            if(redo[redo.length - 1] === 'clear') {
+                clearCanvas();
+                redo.pop();
+                return;
+            }
+            else {
+                canvas.add(redo[redo.length - 1]);
+                redo.pop();
+            }
+        }
+        else {
+            return;
+        }
+    }
+    catch {
+        return;
+    }
+}); 
+
+document.getElementById('erase').addEventListener('click', () => {    
+    mode = 'erase';
+    canvas.isDrawingMode = false;
+    console.log('erase-clicked');
+}); 
+
+document.getElementById('select').addEventListener('click', () => {    
+    mode = 'select';
+    canvas.isDrawingMode = false;
+}); 
+
+canvas.on('mouse:up', function(e) {
+    if(mode === 'erase') {
+        redo.push(e.target);
+        canvas.remove(e.target);
+        console.log('Undo Stack = ', obj);
+    }
+});
+
+canvas.on('mouse:down', () => {
+    if(visible) {
+        document.getElementById('colorpallete').classList.remove('show');
+        visible = false;
+    }
+});
+
+//Color Pallete Toggle
+function togglePallete() {
+    mode = 'draw';
+    canvas.isDrawingMode = true;
+    visible = true;
     document.getElementById('colorpallete').classList.toggle('show');
 };
 
-function hidePallete() {    
-    document.getElementById('colorpallete').classList.remove('show');
-};
 
-window.onbeforeunload = function() {
-    return "you can not refresh the page";
-}
-
-function setColor(event) {
-    penColor = event.target.value;
-    mode = 'draw';
-}
-    
-function mouseDown(e) {
-    var m = getMouse(e, canvas);
-    points.push({
-        x: m.x,
-        y: m.y
-    });
-    started = true;
-    drawPoints(ctx, points, e);
-};
+// window.onbeforeunload = function() {
+//     return "you can not refresh the page";
+// }
 
 
-function mouseMove(e) { 
-    if (started) {
-        ctx.clearRect(0, 0, 1200, 4000);
-        // put back the saved content
-        ctx.drawImage(memCanvas, 0, 0);
-        var m = getMouse(e, canvas);
-        points.push({
-            x: m.x,
-            y: m.y
-        });
-        drawPoints(ctx, points, e);
-    }
-};
 
-function mouseUp(e) { 
-    if (started) {
-        started = false;
-        // When the pen is done, save the resulting context
-        // to the in-memory canvas
-        memCtx.clearRect(0, 0, memCanvas.width, memCanvas.height);
-        memCtx.drawImage(canvas, 0, 0);
-        points = [];
-    }
-};
-
-// clear both canvases!
-function clear() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    memCtx.clearRect(0, 0, memCanvas.width, memCanvas.height);
-};
-
-
-function drawPoints(ctx, points, e) {
-    if(mode === 'draw') {
-        ctx.strokeStyle = penColor;
-        ctx.moveTo(Math.floor(points[0].x), Math.floor(points[0].y));
-        ctx.lineTo(Math.floor(points[0].x), Math.floor(points[0].y));
-        // draw a bunch of quadratics, using the average of two points as the control point
-        for (i = 1; i < points.length - 2; i++) {
-            var c = Math.floor((points[i].x + points[i + 1].x) / 2),
-                d = Math.floor((points[i].y + points[i + 1].y) / 2);
-            ctx.quadraticCurveTo(Math.floor(points[i].x), Math.floor(points[i].y), c, d);
-        }
-        ctx.stroke();
-        ctx.beginPath();
-    }
-    else if(mode === 'erase') {
-        var p = getMouse(e, canvas);
-        ctx.clearRect(p.x - 20, p.y - 20, 60, 60);
-        memCtx.clearRect(p.x - 20, p.y - 20, 60, 60);
-    }
-}
-
-
-// Creates an object with x and y defined,
-// set to the mouse position relative to the state's canvas
-// If you wanna be super-correct this can be tricky,
-// we have to worry about padding and borders
-// takes an event and a reference to the canvas
-function getMouse(e, canvas) {
-  var element = canvas, offsetX = 0, offsetY = 0, mx, my;
-
-  // Compute the total offset. It's possible to cache this if you want
-  if (element.offsetParent !== undefined) {
-    do {
-      offsetX += element.offsetLeft;
-      offsetY += element.offsetTop;
-    } while ((element = element.offsetParent));
-  }
-
-  mx = e.pageX - offsetX;
-  my = e.pageY - offsetY;
-
-  // We return a simple javascript object with x and y defined
-  return {x: mx, y: my};
-}
-
-
+//Brush Color Event-Listeners
 document.getElementById('red').onclick = setColor;
 document.getElementById('blue').onclick = setColor;
 document.getElementById('purple').onclick = setColor;
@@ -159,3 +176,5 @@ document.getElementById('brown').onclick = setColor;
 document.getElementById('black').onclick = setColor;
 document.getElementById('aqua').onclick = setColor;
 document.getElementById('orange').onclick = setColor;
+
+
